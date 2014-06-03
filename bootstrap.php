6931bc2557e1b95d7cea7a4088c5a8d0132cec52
Hung Tran <oohnoitz@gmail.com>
2014-06-02 22:05:50 -0500

    updated for foolfuuka 2.x compat

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,26 @@
 <?php
 
-\Foolz\Plugin\Event::forge('Foolz\Plugin\Plugin::execute.foolz/foolfuuka-plugin-banners')
-	->setCall(function($result) {
-		\Autoloader::add_classes([
-			'Foolz\Foolfuuka\Plugins\Banners\Model\Banners' => __DIR__.'/classes/model/banners.php'
-		]);
+use Foolz\Plugin\Event;
 
-		\Foolz\Plugin\Event::forge(['foolfuuka.themes.default_after_op_open', 'foolfuuka.themes.default_after_headless_open'])
-			->setCall('Foolz\Foolfuuka\Plugins\Banners\Model\Banners::display')
-			->setPriority(5);
-	});
+class HHVM_Banners
+{
+    public function run()
+    {
+        Event::forge('Foolz\Plugin\Plugin::execute.foolz/foolfuuka-plugin-banners')
+            ->setCall(function ($result) {
+                /* @var Context $context */
+                $context = $result->getParam('context');
+                /** @var Autoloader $autoloader */
+                $autoloader = $context->getService('autoloader');
+                $autoloader->addClassMap([
+                    'Foolz\Foolfuuka\Plugins\Banners\Model\Banners' => __DIR__.'/classes/model/banners.php'
+                ]);
+
+                Event::forge(['foolfuuka.themes.default_after_op_open', 'foolfuuka.themes.default_after_headless_open'])
+                    ->setCall('Foolz\Foolfuuka\Plugins\Banners\Model\Banners::display')
+                    ->setPriority(5);
+            });
+    }
+}
+
+(new HHVM_Banners())->run();
--- classes/model/banners.php
+++ classes/model/banners.php
@@ -4,36 +4,21 @@ namespace Foolz\Foolfuuka\Plugins\Banners\Model;
 
 class Banners
 {
-	static $processed = false;
+    static $processed = false;
 
-	public static function display($result)
-	{
-		if (static::$processed === true)
-		{
-			return null;
-		}
+    public static function display($result)
+    {
+        if (static::$processed === true || !is_object(($board = $result->getParam('board', null)))) {
+            return null;
+        }
 
-		if (\Cookie::get('theme') === 'yotsubatwo')
-		{
-			return null;
-		}
+        $banners = glob(__DIR__.'/../../assets/banners/'.$board->shortname.'/*.*');
+        if ($banners !== false && !empty($banners)) {
+            $plugin = $result->getObject()->getContext()->getService('plugins')->getPlugin('foolz/foolfuuka-plugin-ads-and-banners');
 
-		$board = $result->getParam('board');
+            echo '<img src="'.$plugin->getAssetManager()->getAssetLink('banners/'.$board->shortname.'/'.basename($banners[array_rand($banners)])).'" style="float: right; display: block; margin-bottom: 3px; position: relative; bottom: 10px">';
+        }
 
-		if ( ! is_object($board) || ! in_array($board->shortname, ['dev', 'foolz', 'kuku']))
-		{
-			return null;
-		}
-
-		$banners = glob(__DIR__.'/../../assets/banners/'.$board->shortname.'/*.*');
-
-		if ($banners !== false && ! empty($banners))
-		{
-			$plugin = \Plugins::getPlugin('foolz/foolfuuka', 'foolz/foolfuuka-plugin-banners');
-
-			echo '<img src="'.$plugin->getAssetManager()->getAssetLink('banners/'.$board->shortname.'/'.basename($banners[array_rand($banners)])).'" style="float: right; display: block; margin-bottom: 3px; position: relative; bottom: 10px" />';
-		}
-
-		static::$processed = true;
-	}
+        static::$processed = true;
+    }
 }
